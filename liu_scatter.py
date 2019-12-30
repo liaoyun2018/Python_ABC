4884928a5c447924e76000d0d8bd7983ed7415ff
liaoyun2018 <59081998+liaoyun2018@users.noreply.github.com>
2019-12-30 21:35:07 +0800

    Update liu_scatter.py
    散点图中加一条直线，表示指标控制范围。

--- liu_scatter.py
+++ liu_scatter.py
@@ -22,11 +22,14 @@ df = df['硫分\n（%）'].drop([0, 1, 2, 7])
 df = df.astype('float')
 x = df.index.astype('float')
 y1 = df
-print(type(y1), type(x), x, y1)
+# print(len(y1))
+y2 = [0.50] * 29
+# print(type(y1), type(x), x, y1)
 
 plt.scatter(x, y1, label='硫含量')
-
+plt.plot(x, y2, 'r')
 plt.ylabel('硫含量')
-plt.title('煅后焦硫含量数据分析')
+plt.xticks([])  # 不显示x轴刻度
+plt.title('低硫煅后焦硫含量数据分析-201912',  fontsize=18)
 plt.legend()
 plt.show()
